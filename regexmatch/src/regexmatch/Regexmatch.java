0937ca1486f27f7f559a04011900c78db5d86b46
Hanan Abubacar <hannahn006@gmail.com>
2016-09-15 08:21:57 +0800

    adds another regex

--- regexmatch/src/regexmatch/Regexmatch.java
+++ regexmatch/src/regexmatch/Regexmatch.java
@@ -7,9 +7,14 @@ public class Regexmatch {
     public static boolean isTrue(String str){
         return str.matches("[Tt]rue");
     }
+    
+    public static boolean containsFalse(String str){
+        return str.matches(".*false.*");
+    }
+    
     public static void main(String[] args) {
         System.out.println(isTrue("True"));
-        
+        System.out.println(containsFalse("sdfghghgfhjdhds"));
     }
     
 }
