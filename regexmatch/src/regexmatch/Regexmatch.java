a06e5ea562e392fafb091aaabde0ad952ed25ec6
Hanan Abubacar <hannahn006@gmail.com>
2016-09-15 08:14:59 +0800

    Adds first regex

--- regexmatch/src/regexmatch/Regexmatch.java
+++ regexmatch/src/regexmatch/Regexmatch.java
@@ -4,7 +4,11 @@ package regexmatch;
 
 public class Regexmatch {
 
+    public static boolean isTrue(String str){
+        return str.matches("[Tt]rue");
+    }
     public static void main(String[] args) {
+        System.out.println(isTrue("True"));
         
     }
     
